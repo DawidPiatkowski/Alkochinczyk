46d0790b00831da3ffcd2f563c38d23e55305f22
dawid <piatek06892@wp.pl>
2023-09-17 16:11:45 +0200

    Początek

---
+++ alkochinczyk/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alkochinczyk project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alkochinczyk.settings')
+
+application = get_wsgi_application()
---
+++ alkochinczyk_planszowka/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+class Property(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    owner = models.ForeignKey('Player', on_delete=models.CASCADE, null=True, blank=True)
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    money = models.IntegerField(default=1500)
+    position = models.IntegerField(default=0)
